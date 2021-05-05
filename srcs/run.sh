856476c4e622c5abbea5d47186851728b2340ed3
Hyeon yi Nam <hyenam@c5r9s7.42seoul.kr>
2021-05-05 19:09:11 +0900

    a

--- srcs/run.sh
+++ srcs/run.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# nginx 여기서 fail뜨는거 수정하기
-service nginx start
-service nginx status
+#권한 설정
+chmod 775 /run.sh
+chown -R www-data:www-data /var/www/
+chmod -R 755 /var/www
 
 #openssl
-openssl req -x509 -newkey rsa:4096 -nodes -subj '/CN=localhost' -keyout localhost.dev.key -out localhost.dev.crt -days 365
+openssl req -x509 -newkey rsa:4096 -nodes -subj '/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost' -keyout localhost.dev.key -out localhost.dev.crt -days 365
 mv localhost.dev.crt etc/ssl/certs/
 mv localhost.dev.key etc/ssl/private/
 chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
 mv default ./etc/nginx/sites-available/default
-service nginx restart
 
 # php-fpm
 mv /var/www/html/* ./
-service nginx restart
-service php7.3-fpm start
 
 #wordpress
 wget https://wordpress.org/latest.tar.gz
@@ -26,7 +24,6 @@ chown -R www-data:www-data /var/www/html/wordpress
 #mysql
 service mysql start 
 mysql < sql.sh
-service php7.3-fpm restart
 
 # phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
@@ -34,8 +31,11 @@ tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 mv phpmyadmin /var/www/html/
 mv config.inc.php var/www/html/phpmyadmin/
-service nginx restart
+
+service nginx start
+service php7.3-fpm start
 service mysql start
+
 mysql < var/www/html/phpmyadmin/sql/create_tables.sql
 
 bash
